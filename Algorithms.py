e2ccf6111aa352c560c501f12bb08f10e3d16eb1
Nytheas <nytheas@seznam.cz>
2021-04-03 23:09:27 +0200

    vanilla simulatedannealing

--- Algorithms.py
+++ Algorithms.py
@@ -1,3 +1,4 @@
+import math
 import random
 import TFunctions
 from statistics import NormalDist
@@ -42,7 +43,7 @@ class RandomSearch:
             else:
                 print("Špatný algoritmus!!!")
                 vnow = 0
-            self.return_values(randval, vnow, i)
+            # self.return_values(randval, vnow, i)
             if vnow < self.vhodnost:
                 self.vhodnost = vnow
                 self.bestarg = randval[:]
@@ -141,7 +142,7 @@ class LocalSearch:
             if self.tmp_vhodnost <= self.vhodnost:
                 self.bestarg = self.tmp_bestarg[:]
                 self.vhodnost = self.tmp_vhodnost
-            self.return_values(self.bestarg, self.vhodnost, j, i)
+            # self.return_values(self.bestarg, self.vhodnost, j, i)
 
     def return_values(self, val, vnow, neighbours, iters):
         if self.verbose:
@@ -240,7 +241,119 @@ class HillClimbing:
             if self.tmp_vhodnost <= self.vhodnost:
                 self.bestarg = self.tmp_bestarg[:]
                 self.vhodnost = self.tmp_vhodnost
-            self.return_values(self.bestarg, self.vhodnost, j, i)
+            # self.return_values(self.bestarg, self.vhodnost, j, i)
+
+    def return_values(self, val, vnow, neighbours, iters):
+        if self.verbose:
+            row = "%s; %s; %s; %s; %s; %s; %s" % (str(self.id_spusteni), str(neighbours), str(iters),
+                                                  str(self.classname), str(self.algoritmus),
+                                                  str(val), str(vnow))
+        else:
+            row = " %s; %s" % (str(val), str(vnow))
+        if self.file != '':
+            f = open(self.file, 'a')
+            if self.print_all or self.vhodnost >= vnow:
+                f.write("%s\n" % row)
+            f.close()
+        else:
+            if self.print_all or self.vhodnost >= vnow:
+                print("%s" % row)
+
+
+class SimulatedAnnealing:
+    def __init__(self, fes, okoli, dimenze, smodch, algoritmus, pocatecni_teplota, konecna_teplota, redukce_teploty,
+                 verbose=False, print_all=False, file='', id_spusteni=''):
+        self.fes = fes
+        self.okoli = okoli
+        self.soucasne_okoli = okoli
+        self.bestarg = []
+        self.vhodnost = 100000000000
+        self.dimenze = dimenze
+        self.smodch = smodch
+        self.algoritmus = algoritmus
+        self.verbose = verbose
+        self.print_all = print_all
+        self.file = file
+        self.id_spusteni = id_spusteni
+        self.vysledky = []
+        self.pocatecni_teplota = pocatecni_teplota
+        self.soucasna_teplota = pocatecni_teplota
+        self.konecna_teplota = konecna_teplota
+        self.redukce_teploty = redukce_teploty
+
+        if self.algoritmus in ['FirstDeJong', 'SecondDeJong']:
+            self.minnum = -5
+            self.maxnum = 5
+        elif self.algoritmus == 'Schwefel':
+            self.minnum = -500
+            self.maxnum = 500
+        self.vysledky = []
+
+    classname = 'SimulatedAnnealing'
+
+    def generate(self, input_vector):
+        output_vector = []
+
+        if len(input_vector) == 0:
+            for i in range(self.dimenze):
+                output_vector.append(self.minnum + (random.random() * (self.maxnum - self.minnum)))
+        else:
+            for i in range(self.dimenze):
+                valcheck = False
+                while not valcheck:
+                    val = NormalDist(input_vector[i], (self.smodch * (self.maxnum - self.minnum)))
+                    val = val.inv_cdf(random.random())
+                    if self.minnum <= val <= self.maxnum:
+                        valcheck = True
+                output_vector.append(val)
+        return output_vector
+
+    def compute_inner(self, in_bestarg, in_vhodnost):
+        for i in range(self.soucasne_okoli):
+            randval = self.generate(in_bestarg)
+            if self.algoritmus == 'FirstDeJong':
+                vnow = TFunctions.FirstDeJong(randval).compute()
+            elif self.algoritmus == 'SecondDeJong':
+                vnow = TFunctions.SecondDeJong(randval).compute()
+            elif self.algoritmus == 'Schwefel':
+                vnow = TFunctions.Schwefel(randval).compute()
+            else:
+                print("Špatný algoritmus!!!")
+                self.fes = 1
+            self.fes -= 1
+            df = vnow - in_vhodnost
+            if df < 0:
+                p = 1
+            else:
+                p = math.e ** (-df / self.soucasna_teplota)
+
+            # print("In vhodnost: %s, Delta F: %s, pravdepodobnost: %s, soucasna_teplota: %s, zbyva_fes: %s, vhodnost: %s" % (in_vhodnost, df, p, self.soucasna_teplota, self.fes, self.vhodnost))
+            if df < 0:
+                in_vhodnost = vnow
+                in_bestarg = randval[:]
+                if in_vhodnost < self.vhodnost:
+                    self.vhodnost = in_vhodnost
+                    self.bestarg = randval[:]
+            else:
+                gate = math.e ** (-df / self.soucasna_teplota)
+                r = random.random()
+                if r < gate:
+                    in_vhodnost = vnow
+                    in_bestarg = randval[:]
+            self.vysledky.append(self.vhodnost)
+            if self.fes <= 0:
+                return in_bestarg, in_vhodnost
+        return in_bestarg, in_vhodnost
+
+    def compute(self):
+        in_bestarg = self.bestarg[:]
+        in_vhodnost = self.vhodnost
+        while self.fes > 0 and self.soucasna_teplota > self.konecna_teplota:
+            in_bestarg, in_vhodnost = self.compute_inner(in_bestarg, in_vhodnost)
+            self.mezikolove_nastaveni()
+
+    def mezikolove_nastaveni(self):
+        self.soucasna_teplota = self.soucasna_teplota * self.redukce_teploty
 
     def return_values(self, val, vnow, neighbours, iters):
         if self.verbose:
--- Tests.py
+++ Tests.py
@@ -1,5 +1,7 @@
 import Algorithms
 import statistics
+import matplotlib.pyplot as plt
+
 
 class Results:
     def __init__(self, algorithm, function, runtimes, iterations, dimensions):
@@ -22,15 +24,21 @@ class Results:
 
         self.minimum = min(vals)
         self.maximum = max(vals)
-        self.median = statistics.median(vals)
-        self.mean = statistics.mean(vals)
-        self.stdev = statistics.stdev(vals)
+        if len(vals) > 1:
+            self.median = statistics.median(vals)
+            self.mean = statistics.mean(vals)
+            self.stdev = statistics.stdev(vals)
+        else:
+            self.median = min(vals)
+            self.mean = min(vals)
+            self.stdev = 0
 
     def printstats(self):
         text = "algorithm: %s; function: %s; runtimes: %s; minimum: %s; maximum: %s; mean: %s; median: %s; stdev: %s" \
                % (self.algorithm, self.function, self.runtimes, self.minimum, self.maximum, self.mean, self.median,
                   self.stdev)
         return text
+
 
 class OneResult:
     def __init__(self, runid, bestarg, vhodnost, vysledky):
@@ -40,50 +48,105 @@ class OneResult:
         self.vysledky = vysledky
 
 
-
 runtimes = 30
 fes = 10000
 dimensions = 10
 
+talgs = ['RandomSearch', 'LocalSearch', 'Hill Climber']
 tfunctions = ['FirstDeJong', 'SecondDeJong', 'Schwefel']
 
 result_list = {}
 
 
-'RANDOMSEARCH'
-for funct in tfunctions:
-    for run in range(1, runtimes+1):
-        resid = "RandomSearch" + funct
-        if resid not in result_list:
-            result_list[resid] = Results('RandomSearch', funct, runtimes, fes, dimensions)
-        x = Algorithms.RandomSearch(fes, dimensions, funct, verbose=False, print_all=False, file='',
-                                    id_spusteni=run)
-        x.compute()
-        result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
+# 'RANDOMSEARCH'
+# for funct in tfunctions:
+#     for run in range(1, runtimes+1):
+#         resid = "RandomSearch" + funct
+#         if resid not in result_list:
+#             result_list[resid] = Results('RandomSearch', funct, runtimes, fes, dimensions)
+#         x = Algorithms.RandomSearch(fes, dimensions, funct, verbose=False, print_all=False, file='',
+#                                     id_spusteni=run)
+#         x.compute()
+#         result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
+#
+# 'LOCALSEARCH'
+# for funct in tfunctions:
+#     for run in range(1, runtimes+1):
+#         resid = "LocalSearch" + funct
+#         if resid not in result_list:
+#             result_list[resid] = Results('LocalSearch', funct, runtimes, fes, dimensions)
+#         x = Algorithms.LocalSearch(int(fes / 10), 10, dimensions, 0.1, funct, nb_max=1, verbose=False, print_all=False,
+#                                    file='', id_spusteni=run)
+#         x.compute()
+#         result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
+#
+# 'HILLCLIMBING'
+# for funct in tfunctions:
+#     for run in range(1, runtimes+1):
+#         resid = "HillClimbing" + funct
+#         if resid not in result_list:
+#             result_list[resid] = Results('HillClimbing', funct, runtimes, fes, dimensions)
+#         x = Algorithms.LocalSearch(int(fes / 10), 10, dimensions, 0.1, funct, nb_max=0, verbose=False, print_all=False,
+#                                    file='', id_spusteni=run)
+#         x.compute()
+#         result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
+#
+#
+# 'SIMULATEDANNEALING'
+# for funct in tfunctions:
+#     for run in range(1, runtimes+1):
+#         resid = "SimulatedAnnealing" + funct
+#         if resid not in result_list:
+#             result_list[resid] = Results('SimulatedAnnealing', funct, runtimes, fes, dimensions)
+#         x = Algorithms.SimulatedAnnealing(fes, 10, dimensions, 0.1, funct, 1000, 0.1, 0.991, verbose=False,
+#                                           print_all=False, file='', id_spusteni=run)
+#         x.compute()
+#         result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
+#
+# for i in result_list:
+#     result_list[i].count_stats()
+#     print(result_list[i].printstats())
 
-'LOCALSEARCH'
-for funct in tfunctions:
-    for run in range(1, runtimes+1):
-        resid = "LocalSearch" + funct
-        if resid not in result_list:
-            result_list[resid] = Results('LocalSearch', funct, runtimes, fes, dimensions)
-        x = Algorithms.LocalSearch(int(fes / 10), 10, dimensions, 0.1, funct, nb_max=1, verbose=False, print_all=False,
-                                   file='', id_spusteni=run)
-        x.compute()
-        result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
 
-'HILLCLIMBING'
-for funct in tfunctions:
+#
+# xace = []
+# for a in range(1, 10001):
+#     xace.append(a)
+#
+# fun = []
+#
+# for i in result_list['RandomSearchFirstDeJong'].instances:
+#     plt.plot(x, result_list['RandomSearchFirstDeJong'].instances[i].vysledky)
+# plt.savefig('test.png')
+# plt.close()
+# for i in result_list['RandomSearchSecondDeJong'].instances:
+#     plt.plot(x, result_list['RandomSearchSecondDeJong'].instances[i].vysledky)
+# plt.savefig('test2.png')
+#
+# #
+#
+runtimes = 1
+'SIMULATEDANNEALING'
+for funct in ['FirstDeJong']:
     for run in range(1, runtimes+1):
-        resid = "HillClimbing" + funct
+        resid = "SimulatedAnnealing" + funct
         if resid not in result_list:
-            result_list[resid] = Results('HillClimbing', funct, runtimes, fes, dimensions)
-        x = Algorithms.LocalSearch(int(fes / 10), 10, dimensions, 0.1, funct, nb_max=0, verbose=False, print_all=False,
-                                   file='', id_spusteni=run)
+            result_list[resid] = Results('SimulatedAnnealing', funct, runtimes, fes, dimensions)
+        x = Algorithms.SimulatedAnnealing(fes, 10, dimensions, 0.1, funct, 1000, 0.1, 0.991, verbose=False,
+                                          print_all=False, file='', id_spusteni=run)
+
         x.compute()
         result_list[resid].instances[run] = OneResult(run, x.bestarg[:], x.vhodnost, x.vysledky[:])
+        print("TEPLOTA:", x.soucasna_teplota, "FES", x.fes)
 
 for i in result_list:
     result_list[i].count_stats()
     print(result_list[i].printstats())
+
+y = result_list['SimulatedAnnealingFirstDeJong'].instances[1].vysledky
+#
+#
+#
+# plt.plot(xace, y)
+# plt.savefig('test.png')
 
